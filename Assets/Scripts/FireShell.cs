f91c6e90f1a2bacf5096f282ef0f089ba95dfbaf
AndrePerezIPB <148081475+AndrePerezIPB@users.noreply.github.com>
2023-11-05 21:27:05 +0000

    1&2.Calculating Trajectories Part 1 and 2
    Calculating rotation of the AI tank and turret

--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -7,51 +7,64 @@ public class FireShell : MonoBehaviour {
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
+    public Transform turretBase;
+    float speed = 15.0f;
+    float rotSpeed = 2.0f;
 
     void CreateBullet() {
 
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
-    void Update() {
-
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-
-            Vector3 aimAt = CalculateTrajectory();
-            if (aimAt != Vector3.zero) {
-
-                this.transform.forward = CalculateTrajectory();
-                CreateBullet();
-            }
+    void RotateTurret()
+    {
+        float? angle = CalculateAngle(true);
+        if (angle != null)
+        {
+            turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
         }
     }
 
-    Vector3 CalculateTrajectory() {
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = enemy.transform.position - this.transform.position;
+        float y = targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float sSqr = speed * speed;
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
 
-        Vector3 p = enemy.transform.position - this.transform.position;
-        Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
-        float s = bullet.GetComponent<MoveShell>().speed;
+        if (underTheSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underTheSqrRoot);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
 
-        float a = Vector3.Dot(v, v) - s * s;
-        float b = Vector3.Dot(p, v);
-        float c = Vector3.Dot(p, p);
-        float d = b * b - a * c;
-
-        if (d < 0.1f) return Vector3.zero;
-
-        float sqrt = Mathf.Sqrt(d);
-        float t1 = (-b - sqrt) / c;
-        float t2 = (-b + sqrt) / c;
-
-        float t = 0.0f;
-        if (t1 < 0.0f && t2 < 0.0f) return Vector3.zero;
-        else if (t1 < 0.0f) t = t2;
-        else if (t2 < 0.0f) t = t1;
-        else {
-
-            t = Mathf.Max(new float[] { t1, t2 });
+            if (low)
+            {
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            }
+            else
+            {
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+            }
         }
-        return t * p + v;
+        else
+        {
+            return null;
+        }
+    }
+
+    void Update() 
+    {
+        Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+        RotateTurret();
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            CreateBullet();
+        }
     }
 }
